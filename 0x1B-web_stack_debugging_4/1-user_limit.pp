6252d6dc14cec643058dc976d6d2c4859374eaa7
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-10-19 13:02:54 -0700

    push

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,4 @@
-# Ensure that the user 'holberton' can log in and open files without encountering errors.
-
-# Increase the hard file limit for the 'holberton' user in /etc/security/limits.conf.
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf', # Replace the hard file limit value for 'holberton' with 50000.
-  path    => '/usr/local/bin/:/bin/'
-}
-
-# Increase the soft file limit for the 'holberton' user in /etc/security/limits.conf.
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf', # Replace the soft file limit value for 'holberton' with 50000.
-  path    => '/usr/local/bin/:/bin/'
+# Fix files
+exec { 'Fix hard limit':
+  command => '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf'
 }
