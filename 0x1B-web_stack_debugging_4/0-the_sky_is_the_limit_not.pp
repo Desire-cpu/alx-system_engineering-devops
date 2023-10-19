11110c75488bdb2c1b60ec9bc687e1712261710c
junior sibusisos ngondo <juniorthemba1@gmail.com>
2023-10-19 12:59:33 -0700

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,5 @@
-# Increases the ULIMIT of the default file for the Nginx server.
-
-# Increase the ULIMIT in the default Nginx configuration file.
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx', # Replace the ULIMIT value from 15 to 4096.
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx after changing the ULIMIT.
-exec { 'nginx-restart':
-  command => 'nginx restart', # Restart the Nginx server.
-  path    => '/etc/init.d/'
+# Fix Nginx limits
+exec { 'Limit':
+  command => '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx',
 }
+exec { '/usr/bin/env service nginx restart': }
